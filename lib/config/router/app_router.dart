ea8c5e8e468e1452d1c2884b9c3cf6756cbc71a5
Erick Andrade <grillo.erick1@gmail.com>
2023-07-04 19:53:51 -0600

    rutas con nombre implementado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -6,13 +6,16 @@ final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
       path: '/buttons',
+      name: ButtonsScreen.name,
       builder: (context, state) => const ButtonsScreen(),
     ),
     GoRoute(
+      name: CardsScreen.name,
       path: '/cards',
       builder: (context, state) => const CardsScreen(),
     ),
