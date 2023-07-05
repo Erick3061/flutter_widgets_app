f0a401fac82888fadfb549f569b341d204a338ea
Erick Andrade <grillo.erick1@gmail.com>
2023-07-04 21:38:12 -0600

    terminar creacion de directorios....

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AniamtedScreen extends StatelessWidget {
+  static const String name = 'AniamtedScreen';
+  const AniamtedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
